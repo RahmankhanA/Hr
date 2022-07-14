3547d81baf58e625bdcd364f5fb36c31986fd1cb
prashant8746 <86104333+prashant8746@users.noreply.github.com>
2022-07-14 14:18:36 +0530

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,11 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // rgba(14,67,117,255)
+    
 
         primarySwatch:
             MaterialColor(Color.fromRGBO(14, 67, 117, 1).value, color),
-        // primarySwatch: const Color.fromARGB(14,67,117,255),
+      ,
       ),
       home: const Home(),
     );
